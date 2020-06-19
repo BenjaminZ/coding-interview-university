c1730179f5e42e5f21bd8592f749ca49c35ca71d
Chibin Zhang <benjaminzhang1130@gmail.com>
2020-06-19 23:19:11 +1000

    working on minigrep

---
+++ learn-rust/minigrep/src/main.rs
@@ -0,0 +1,21 @@
+use std::env;
+use std::process;
+
+use minigrep::*;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+    println!("Query is {}", config.query);
+    println!("Filename is {}\n", config.filename);
+
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
+}
+
